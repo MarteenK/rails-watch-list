63cefb7fe6c84b8ad7cc71e80a1d2e37cf0ea2a9
martin karlsson <markarlsson@gmail.com>
2021-06-25 15:58:08 -0300

    work in progress

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,7 @@
 class Bookmark < ApplicationRecord
-  belongs_to :list
   belongs_to :movie
+  belongs_to :list
 
-  validates :comment, lenght: { minimum: 6 }
-  validates :movie_id, uniqueness: { scope: :list_id, message: 'Bookmark is not unique'}
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id, message: "is already in the list" }
 end
