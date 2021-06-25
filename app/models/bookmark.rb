500ed21e36a72377a1f7811d01d7380350c5d549
martin karlsson <markarlsson@gmail.com>
2021-06-25 13:02:18 -0300

    first migrate

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,4 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
 
   validates :comment, lenght: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'Bookmark is not unique'}
 end
